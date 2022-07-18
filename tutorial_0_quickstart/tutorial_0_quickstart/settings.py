7d68de12bc0d2bb87f0dffca68e4f2e754fdf155
kuga-github <kuga.tsunekawa@gmail.com>
2022-07-18 19:25:26 +0900

    tutorial_0_quickstart

--- tutorial_0_quickstart/tutorial_0_quickstart/settings.py
+++ tutorial_0_quickstart/tutorial_0_quickstart/settings.py
@@ -11,19 +11,22 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(DEBUG=(bool,False))
+env.read_env(str(BASE_DIR) + '/.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rl6$ct5qzfn=fcgxg(@htzj)th#veuv8!p^d1bkkpak3&i_o)b'
+SECRET_KEY = env('SECRET_KEY', str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG', bool)
 
 ALLOWED_HOSTS = []
 
